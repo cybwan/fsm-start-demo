56201b44b9a83641ff5dd32b108573c2cecaf166
Cybwan <baili@flomesh.io>
2024-08-12 16:52:07 +0800

    update ztm demo.

--- scripts/deploy-fsm.sh
+++ scripts/deploy-fsm.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 CTR_REGISTRY="${CTR_REGISTRY:-cybwan}"
-CTR_TAG="${CTR_TAG:-1.4.0-alpha.3}"
+CTR_TAG="${CTR_TAG:-1.4.0-alpha.4}"
 IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-Always}"
 
 fsm_namespace="${fsm_namespace:-fsm-system}"
